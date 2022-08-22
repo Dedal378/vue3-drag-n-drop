c45cf1c7ddbfaac066c45805eb4f01900f93d5c1
Alex <41344035+Dedal378@users.noreply.github.com>
2022-08-22 20:45:07 +0300

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a
+        href="https://vitejs.dev/"
+        target="_blank"
+      >
+        Vite
+      </a>
+      +
+      <a
+        href="https://vuejs.org/"
+        target="_blank"
+      >
+        Vue 3.
+      </a>
+    </h3>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  top: -10px;
+  font-size: 2.6rem;
+  font-weight: 500;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
